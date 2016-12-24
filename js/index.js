006da6ee7d65a964e43d187443f26b5a25922350
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-12-24 22:41:29 +0000

    Working on snowflake shape

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var SNOWFLAKE_COLOUR = 0xFFFFFF;
 var SNOWFLAKE_RATE_DEFAULT = 10;
-var SNOWFLAKE_SIZE = 50;
+var SNOWFLAKE_SIZE = 30;
+var TRIANGLE_LENGTH = SNOWFLAKE_SIZE / 3;
 var SNOWFLAKE_SPEED = 1;
 
 var snowflakeRate = SNOWFLAKE_RATE_DEFAULT;
@@ -9,7 +10,7 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 var container = document.getElementById('container');
 
-var renderer = new PIXI.autoDetectRenderer(width, height, {transparent: true});
+var renderer = new PIXI.autoDetectRenderer(width, height, {transparent: false, antialias: true});
 
 var stage = new PIXI.Container();
 var snowflakes = [];
@@ -17,6 +18,8 @@ var snowflakes = [];
 init();
 
 function init() {
+
+  renderer.backgroundColor = 0x987fff;
 
   container.style.width = width;
   container.style.height = height;
@@ -32,18 +35,41 @@ function init() {
 
 }
 
+/**
+ * We're making the second shape shown here:
+ * http://mathworld.wolfram.com/KochSnowflake.html
+ */
 function createSnowflake() {
 
-  var triangle = new PIXI.Graphics();
+  var snowflake = new PIXI.Graphics();
+
+  // Start at the edge of the top left triangle
+  // a^2 + b^2 = c^2, a=TRIANGLE_LENGTH/2, b=?, c=SNOWFLAKE_TRIANGLE
+  var triangleHeight = Math.sqrt((TRIANGLE_LENGTH/2 * TRIANGLE_LENGTH/2) + (TRIANGLE_LENGTH * TRIANGLE_LENGTH));
+  var pos = {x: 0, y: triangleHeight};
+
+  snowflake.lineStyle(2, SNOWFLAKE_COLOUR, 1);
+  snowflake.moveTo(pos.x, pos.y);
+  snowflake.lineTo(pos.x += TRIANGLE_LENGTH, pos.y);
+  snowflake.lineTo(pos.x += TRIANGLE_LENGTH/2, pos.y -= triangleHeight);
+  snowflake.lineTo(pos.x += TRIANGLE_LENGTH/2, pos.y += triangleHeight);
+  snowflake.lineTo(pos.x += TRIANGLE_LENGTH, pos.y);
+  snowflake.lineTo(pos.x -= TRIANGLE_LENGTH/2, pos.y += TRIANGLE_LENGTH);
+  snowflake.lineTo(pos.x += TRIANGLE_LENGTH/2, pos.y += TRIANGLE_LENGTH);
+  snowflake.lineTo(pos.x -= TRIANGLE_LENGTH, pos.y);
+  snowflake.lineTo(pos.x -= TRIANGLE_LENGTH/2, pos.y += TRIANGLE_LENGTH);
+  snowflake.lineTo(pos.x -= TRIANGLE_LENGTH/2, pos.y -= TRIANGLE_LENGTH);
+  snowflake.lineTo(pos.x -= TRIANGLE_LENGTH, pos.y);
+  snowflake.lineTo(pos.x += TRIANGLE_LENGTH/2, pos.y -= TRIANGLE_LENGTH);
+  snowflake.lineTo(pos.x -= TRIANGLE_LENGTH/2, pos.y -= TRIANGLE_LENGTH);
+
   var pos = getRandomStartingPosition();
+  snowflake.position.x = pos.x;
+  snowflake.position.y = pos.y;
 
-  triangle.lineStyle(5, SNOWFLAKE_COLOUR, 1);
-  triangle.moveTo(pos.x, pos.y);
-  triangle.lineTo(pos.x + SNOWFLAKE_SIZE, pos.y + SNOWFLAKE_SIZE);
-  triangle.lineTo(pos.x, pos.y + SNOWFLAKE_SIZE);
-  triangle.lineTo(pos.x, pos.y);
+  snowflake.rotation = getRandomStartingRotation();
 
-  return triangle;
+  return snowflake;
 
 }
 
@@ -59,6 +85,10 @@ function getRandomStartingPosition() {
 
   return {x: x, y: y};
 
+}
+
+function getRandomStartingRotation() {
+  return Math.random() * Math.PI * 2;
 }
 
 function updateOnFrame() {
